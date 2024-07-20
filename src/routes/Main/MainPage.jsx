509980a4a94d3366732d7a65a10ee81ef54e7237
Murodjon Halilov <murodjonhalilov76@gmail.com>
2024-07-21 01:51:50 +0500

    fixed userdata

--- src/routes/Main/MainPage.jsx
+++ src/routes/Main/MainPage.jsx
@@ -3,16 +3,14 @@ import Header from "../../components/Header/Header";
 import MainContainer from "../../components/MainContainer/MainContainer";
 
 const MainPage = () => {
-  const [userData, setUserData] = useState([
-    {
-      id: 1381672171,
-      is_bot: false,
-      first_name: "Murodjon",
-      last_name: "Halilov",
-      username: "MurodjonHalilov",
-      language_code: "en",
-    },
-  ]);
+  const [userData, setUserData] = useState({
+    id: 1381672171,
+    is_bot: false,
+    first_name: "Murodjon",
+    last_name: "Halilov",
+    username: "MurodjonHalilov",
+    language_code: "en",
+  });
 
   useEffect(() => {
     fetch(`http://localhost:8000/usersData`, { method: "GET" })
